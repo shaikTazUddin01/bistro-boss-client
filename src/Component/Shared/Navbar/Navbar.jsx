351295b305d66bce9d22956f1f2d762505069dea
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-01-15 22:54:07 +0600

    added banner slider

--- src/Component/Shared/Navbar/Navbar.jsx
+++ src/Component/Shared/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-[#040404a9] fixed z-10  text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayOut from '../layout/MainLayOut';
+import Home from '../Page/Home/Home';
 
 const Routers = createBrowserRouter([
     {
         path: '/',
-        element: <MainLayOut></MainLayOut>
+        element: <MainLayOut></MainLayOut>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
     }
 ])
 export default Routers;
